189ac988c6c9817e227a21b0cea3819b85ad3e75
Henrik <henke96@bredband.net>
2018-11-17 00:53:43 +0100

    temp lookat

--- js/webgl.js
+++ js/webgl.js
@@ -2,6 +2,112 @@
 window.onload = gInit;
 const HTML_CANVAS = "glCanvas";
 
+function lookAt(out, eye, center, up) {
+  let x0, x1, x2, y0, y1, y2, z0, z1, z2, len;
+  let eyex = eye[0];
+  let eyey = eye[1];
+  let eyez = eye[2];
+  let upx = up[0];
+  let upy = up[1];
+  let upz = up[2];
+  let centerx = center[0];
+  let centery = center[1];
+  let centerz = center[2];
+
+  z0 = eyex - centerx;
+  z1 = eyey - centery;
+  z2 = eyez - centerz;
+
+  len = 1 / Math.sqrt(z0 * z0 + z1 * z1 + z2 * z2);
+  z0 *= len;
+  z1 *= len;
+  z2 *= len;
+
+  x0 = upy * z2 - upz * z1;
+  x1 = upz * z0 - upx * z2;
+  x2 = upx * z1 - upy * z0;
+  len = Math.sqrt(x0 * x0 + x1 * x1 + x2 * x2);
+  if (!len) {
+    x0 = 0;
+    x1 = 0;
+    x2 = 0;
+  } else {
+    len = 1 / len;
+    x0 *= len;
+    x1 *= len;
+    x2 *= len;
+  }
+
+  y0 = z1 * x2 - z2 * x1;
+  y1 = z2 * x0 - z0 * x2;
+  y2 = z0 * x1 - z1 * x0;
+
+  len = Math.sqrt(y0 * y0 + y1 * y1 + y2 * y2);
+  if (!len) {
+    y0 = 0;
+    y1 = 0;
+    y2 = 0;
+  } else {
+    len = 1 / len;
+    y0 *= len;
+    y1 *= len;
+    y2 *= len;
+  }
+
+  out[0] = x0;
+  out[1] = y0;
+  out[2] = z0;
+  out[3] = 0;
+  out[4] = x1;
+  out[5] = y1;
+  out[6] = z1;
+  out[7] = 0;
+  out[8] = x2;
+  out[9] = y2;
+  out[10] = z2;
+  out[11] = 0;
+  out[12] = -(x0 * eyex + x1 * eyey + x2 * eyez);
+  out[13] = -(y0 * eyex + y1 * eyey + y2 * eyez);
+  out[14] = -(z0 * eyex + z1 * eyey + z2 * eyez);
+  out[15] = 1;
+
+  return out;
+}
+
+function multiply(out, a, b, bOffset) {
+  let a00 = a[0], a01 = a[1], a02 = a[2], a03 = a[3];
+  let a10 = a[4], a11 = a[5], a12 = a[6], a13 = a[7];
+  let a20 = a[8], a21 = a[9], a22 = a[10], a23 = a[11];
+  let a30 = a[12], a31 = a[13], a32 = a[14], a33 = a[15];
+
+  // Cache only the current line of the second matrix
+  let b0  = b[0 + bOffset], b1 = b[1 + bOffset], b2 = b[2 + bOffset], b3 = b[3 + bOffset];
+  out[0 + bOffset] = b0*a00 + b1*a10 + b2*a20 + b3*a30;
+  out[1 + bOffset] = b0*a01 + b1*a11 + b2*a21 + b3*a31;
+  out[2 + bOffset] = b0*a02 + b1*a12 + b2*a22 + b3*a32;
+  out[3 + bOffset] = b0*a03 + b1*a13 + b2*a23 + b3*a33;
+
+  b0 = b[4 + bOffset]; b1 = b[5 + bOffset]; b2 = b[6 + bOffset]; b3 = b[7 + bOffset];
+  out[4 + bOffset] = b0*a00 + b1*a10 + b2*a20 + b3*a30;
+  out[5 + bOffset] = b0*a01 + b1*a11 + b2*a21 + b3*a31;
+  out[6 + bOffset] = b0*a02 + b1*a12 + b2*a22 + b3*a32;
+  out[7 + bOffset] = b0*a03 + b1*a13 + b2*a23 + b3*a33;
+
+  b0 = b[8 + bOffset]; b1 = b[9 + bOffset]; b2 = b[10 + bOffset]; b3 = b[11 + bOffset];
+  out[8 + bOffset] = b0*a00 + b1*a10 + b2*a20 + b3*a30;
+  out[9 + bOffset] = b0*a01 + b1*a11 + b2*a21 + b3*a31;
+  out[10 + bOffset] = b0*a02 + b1*a12 + b2*a22 + b3*a32;
+  out[11 + bOffset] = b0*a03 + b1*a13 + b2*a23 + b3*a33;
+
+  b0 = b[12 + bOffset]; b1 = b[13 + bOffset]; b2 = b[14 + bOffset]; b3 = b[15 + bOffset];
+  out[12 + bOffset] = b0*a00 + b1*a10 + b2*a20 + b3*a30;
+  out[13 + bOffset] = b0*a01 + b1*a11 + b2*a21 + b3*a31;
+  out[14 + bOffset] = b0*a02 + b1*a12 + b2*a22 + b3*a32;
+  out[15 + bOffset] = b0*a03 + b1*a13 + b2*a23 + b3*a33;
+  return out;
+}
+
+
 //{ Game - g
 const gVertexShaderSource = `
 	precision mediump float;
@@ -53,6 +159,8 @@ function gInit() {
 		y: 0,
 		z: 0
 	};
+	gViewMatrix = [];
+	lookAt(gViewMatrix, [4, 3, 2], [0, 0, -10], [0, 1, 0]);
 	gInitModels();
 	gInitArrays();
 	gInitObjects();
@@ -227,6 +335,7 @@ var gProgramInfo;
 var gVertexArray;
 var gModels;
 var gCamera;
+var gViewMatrix;
 //}
 //{ Model - model
 function Model(vertices, indices, drawOperation) {
@@ -250,10 +359,10 @@ Model.prototype.drawObjects = function() {
 	const mvps = mat4ArrayCreateIdentities(numObjects);
 	for (let i = 0; i < numObjects; ++i) {
 		let object = this.gameObjects[i];
-		mat4ArrayTranslation(mvps, i, object.x - gCamera.x, object.y - gCamera.y, object.z - gCamera.z);
+		mat4ArrayTranslation(mvps, i, object.x, object.y, object.z);
+		multiply(mvps, gViewMatrix, mvps, i << 4);
 	}
 	gl.bufferData(gl.ARRAY_BUFFER, mvps, gl.DYNAMIC_DRAW);
-	console.log(this);
 	gl.drawElementsInstanced(this.drawOperation, this.indices.length, gl.UNSIGNED_SHORT, 2*this.startElementIndex, numObjects);
 }
 //}
